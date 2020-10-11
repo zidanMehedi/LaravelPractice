9f0f7bfa66a160a56bf3900ab3893303c7e81571
Zidan Mehedi <55471629+zidanMehedi@users.noreply.github.com>
2020-10-11 19:15:26 +0600

    Admin and Customer model created,factories created and seeders created

---
+++ database/migrations/2014_10_11_000000_create_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name', 10); //adding name column inside table with string length 10
+            $table->timestamps();
+        });
+
+    	//inserting default instances inside role table using query builder
+    	DB::table('roles')->insert(['name'=>'admin']);
+    	DB::table('roles')->insert(['name'=>'customer']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
---
+++ database/migrations/2020_10_10_145455_create_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedTinyInteger('role_id'); //foreign key
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles'); //assigning role_id as foreign key of primary key id in roles table 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
